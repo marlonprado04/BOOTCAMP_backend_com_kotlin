5dbdf116ac8407918cd326ab802cc61a780d4137
Marlon Silva do Prado <marlon-prado@SEPLAG.INTRA.RS.GOV.BR>
2023-12-19 17:55:48 -0300

    feat: add 2 first files of code

---
+++ 06_CURSO_introducao_pratica_kotlin/01_hello_world.kt
@@ -0,0 +1,10 @@
+// Códigos Kotlin são agrupados em pacotes. Abaixo tem um pacote sendo definido para o código atual
+// Caso não seja informado o pacote, o código ficará armazenado no pacote padrão
+package org.kotlinlang.play      // 1. Definição do pacote
+
+// Desde o Kotlin 1.3 não é necessário passar nenhum parâmetro na função principal (main)
+// Antes era necessário, por exemplo: "fun main(args: Array<String>)"
+fun main() {         // 2. Ponto de entrada do código em Kotlin        
+    // println imprime uma linha na saída padrão do Kotlin e ela já é importada automaticamente        
+    println("Hello, World!")   // 3. Imprime a saída padrão em um texto pulando uma linha
+}
