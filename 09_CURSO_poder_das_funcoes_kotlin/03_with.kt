78afec43e80c1fb42643cbf69a618df306d4d9fb
Marlon Prado - SPGG <marlonprado04@gmail.com>
2024-01-05 17:29:14 -0300

    feat: add with function examples

---
+++ 09_CURSO_poder_das_funcoes_kotlin/03_with.kt
@@ -0,0 +1,22 @@
+// Cria classe de configuração com o valor host e port
+class Configuration(val host: String, val port: Int) 
+
+fun main() {
+    // Cria instância da classe configuration
+    val configuration = Configuration(host = "127.0.0.1", port = 9000) 
+
+	// Acessa o objeto com o with
+    with(configuration) {
+        // Printa informações do objeto
+        println("$host:$port")
+    }
+
+    // Forma alternativa
+    println("${configuration.host}:${configuration.port}")    
+    
+    // Mais uma alternativa
+    configuration.run{
+        println("$host:$port")
+    }
+    
+}
