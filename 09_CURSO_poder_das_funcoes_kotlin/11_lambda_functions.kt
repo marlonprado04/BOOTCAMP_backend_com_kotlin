7ea17e0b3977edf87fd1e42533b04ddd951a84d7
Marlon Prado - SPGG <marlonprado04@gmail.com>
2024-01-15 16:27:59 -0300

    feat: add lambda function examples

---
+++ 09_CURSO_poder_das_funcoes_kotlin/11_lambda_functions.kt
@@ -0,0 +1,17 @@
+fun main() {
+
+    // Todos os exemplos abaixo criam uma função para deixar as strings em uppercase
+    val upperCase1: (String) -> String = { str: String -> str.uppercase() }
+    val upperCase2: (String) -> String = { str -> str.uppercase() }
+    val upperCase3 = { str: String -> str.uppercase() }
+    // val upperCase4 = { str -> str.uppercase() }
+    val upperCase5: (String) -> String = { it.uppercase() }
+    val upperCase6: (String) -> String = String::uppercase
+
+    println(upperCase1("hello"))
+    println(upperCase2("hello"))
+    println(upperCase3("hello"))
+    println(upperCase5("hello"))
+    println(upperCase6("hello"))
+
+}
